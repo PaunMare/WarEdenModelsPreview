e1b67da7a0c3d178f581af365b25218a3950584f
Marko Paunovic <32879660+PaunMare@users.noreply.github.com>
2022-07-16 16:43:04 +0200

    valjda ce da radi

--- js/about.js
+++ js/about.js
@@ -2,6 +2,7 @@ let option0 = document.getElementById('option0');
 let option1 = document.getElementById('option1');
 let option2 = document.getElementById('option2');
 let option3 = document.getElementById('option3');
+let aboutSection = document.getElementById('about-section');
 
 var myCarousel = document.querySelector('#carouselExampleFade')
 var carousel = new bootstrap.Carousel(myCarousel);
@@ -21,3 +22,19 @@ option2.addEventListener("click", function(){
 option3.addEventListener("click", function(){
     carousel.to('3');
 });
+
+// let scrollCount = 0;
+
+// aboutSection.addEventListener('wheel', function (event) {
+//     if (event.deltaY > 0) {
+//         scrollCount++;
+//         if(scrollCount >= 3)
+//             scrollCount = 3;
+//     }
+//     else if (event.deltaY < 0) {
+//         scrollCount--;
+//         if(scrollCount <=0)
+//             scrollCount = 0;
+//     }
+//     carousel.to(scrollCount.toString());
+// });
--- js/race.js
+++ js/race.js
@@ -1,26 +1,50 @@
 import Splide from '@splidejs/splide';
 
-let humanIcon = document.getElementById('race-human');
-let dwarfIcon = document.getElementById('race-dwarf');
-let elfIcon = document.getElementById('race-elf');
-
+let sliderIcons = document.getElementsByClassName('raceIcon');
 let slider = document.getElementById('splide-race');
 
 slider = new Splide('#splide-race',{
     padding: '10%',
     arrows: false,
     pagination: false,
-    drag   : 'free',
 }).mount();
 
-humanIcon.addEventListener("click", function(){
-    slider.go(0);
-})
+for (let index = 0; index < sliderIcons.length; index++) {
+    const element = sliderIcons[index];
+    const raceName = element.id.split('race-');
 
-dwarfIcon.addEventListener("click", function(){
-    slider.go(1);
-})
+    element.src = "../img/Icons/race/Icon_" + raceName[1] + "_Off.png";
 
-elfIcon.addEventListener("click", function(){
-    slider.go(2);
-})
+    element.addEventListener("click", function(){
+        slider.go(index);
+        clearImages(slider.index);
+        element.src = "../img/Icons/race/Icon_" + raceName[1] + "_On.png";
+    })
+
+    element.addEventListener("mouseover", function(){
+        if(slider.index != index)
+        element.src = "../img/Icons/race/Icon_" + raceName[1] + "_Hover.png";;
+    })
+
+    element.addEventListener("mouseleave", function(){
+        if(slider.index != index)
+        element.src = "../img/Icons/race/Icon_" + raceName[1] + "_Off.png";
+    })  
+}
+
+sliderIcons[0].src = "../img/Icons/race/Icon_Human_On.png";
+
+function clearImages(slideIndex){
+    for (let index = 0; index < sliderIcons.length; index++) {
+        const element = sliderIcons[index];
+        const raceName = element.id.split('race-');
+        if(index != slideIndex)
+            element.src = "../img/Icons/race/Icon_" + raceName[1] + "_Off.png";
+    }
+}
+
+slider.on( 'move', function () {
+    clearImages(slider.index);
+    const raceName = sliderIcons[slider.index].id.split('race-');
+    sliderIcons[slider.index].src = "../img/Icons/race/Icon_" + raceName[1] + "_ON.png";
+} );

3334e5696ff75c3e37ced9e579bd0a2e4f2770af
Marko Paunovic <32879660+PaunMare@users.noreply.github.com>
2022-09-02 10:44:24 +0200

    fix

--- js/about.js
+++ js/about.js
@@ -10,6 +10,8 @@ let image0 = document.getElementById('tab-icon-1');
 let image1 = document.getElementById('tab-icon-2');
 let image2 = document.getElementById('tab-icon-3');
 let image3 = document.getElementById('tab-icon-4');
+let aboutheading = document.getElementById('about-underheading');
+aboutheading.innerHTML = "SKILL-BASED BATTLEGROUND";
 
 image0.src = '../img/Icon_Batleground_On.png';
 image1.src = '../img/Icon_Play.png';
@@ -22,64 +24,68 @@ var myCarousel = document.querySelector('#carouselExampleFade')
 var carousel = new bootstrap.Carousel(myCarousel);
 
 option0.addEventListener("click", function () {
+    aboutheading.innerHTML = "SKILL-BASED BATTLEGROUND";
     carousel.to('0');
 });
 
 option1.addEventListener("click", function () {
+    aboutheading.innerHTML = "FREE TO PLAY AND NO PAY TO WIN";
     carousel.to('1');
 });
 
 option2.addEventListener("click", function () {
+    aboutheading.innerHTML = "CUSTOM-MADE TECH SOLUTIONS";
     carousel.to('2');
 });
 
 option3.addEventListener("click", function () {
+    aboutheading.innerHTML = "IMMUTABLE X / ETHEREUM";
     carousel.to('3');
 });
 
 option0.addEventListener("mouseenter", function () {
-    if(!radio0selected)
+    if (!radio0selected)
         image0.src = '../img/Icon_Batleground_On.png';
 });
 
 option1.addEventListener("mouseenter", function () {
-    if(!radio1selected)
+    if (!radio1selected)
         image1.src = '../img/Icon_Play_On.png';
 
 });
 
 option2.addEventListener("mouseenter", function () {
-    if(!radio2selected)
+    if (!radio2selected)
         image2.src = '../img/Icon_Tech_On.png';
 
 });
 
 option3.addEventListener("mouseenter", function () {
-    if(!radio3selected)
+    if (!radio3selected)
         image3.src = '../img/Icon_Immutable_On.png';
 
 });
 
 option0.addEventListener("mouseleave", function () {
-    if(!radio0selected)
+    if (!radio0selected)
         image0.src = '../img/Icon_Batleground.png';
 
 });
 
 option1.addEventListener("mouseleave", function () {
-    if(!radio1selected)
+    if (!radio1selected)
         image1.src = '../img/Icon_Play.png';
 
 });
 
 option2.addEventListener("mouseleave", function () {
-    if(!radio2selected)
+    if (!radio2selected)
         image2.src = '../img/Icon_Tech.png';
 
 });
 
 option3.addEventListener("mouseleave", function () {
-    if(!radio3selected)
+    if (!radio3selected)
         image3.src = '../img/Icon_Immutable.png';
 
 });
@@ -109,23 +115,31 @@ myCarousel.addEventListener('slide.bs.carousel', function (e) {
     }
     switch (e.to) {
         case 0: {
-            image0.src = '../img/Icon_Batleground_On.png';
             radio0.checked = true;
+            image0.src = '../img/Icon_Batleground_On.png';
+            aboutheading.innerHTML = "SKILL-BASED BATTLEGROUND";
+
             radio0selected = true;
         } break;
         case 1: {
             radio1.checked = true;
             image1.src = '../img/Icon_Play_On.png';
+            aboutheading.innerHTML = "FREE TO PLAY AND NO PAY TO WIN";
+
             radio1selected = true;
         } break;
         case 2: {
             radio2.checked = true;
             image2.src = '../img/Icon_Tech_On.png';
+            aboutheading.innerHTML = "CUSTOM-MADE TECH SOLUTIONS";
+
             radio2selected = true;
         } break;
         case 3: {
             radio3.checked = true;
             image3.src = '../img/Icon_Immutable_On.png';
+            aboutheading.innerHTML = "IMMUTABLE X / ETHEREUM";
+
             radio3selected = true;
         } break;
     }
